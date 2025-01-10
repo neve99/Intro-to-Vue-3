2891c515bed95be976921eaa22432f9f54b178ae
neve99 <165163178+neve99@users.noreply.github.com>
2025-01-10 14:02:36 +0100

    conditional rendering

--- main.js
+++ main.js
@@ -1,7 +1,10 @@
 const app = Vue.createApp({
-    data() {
-        return {
-            product: 'Socks'
-        }
+  data() {
+    return {
+      product: 'Socks',
+      img: './assets/images/socks_green.jpg',
+      inventory: 0,
+      onSale: true,
     }
+  }
 })
